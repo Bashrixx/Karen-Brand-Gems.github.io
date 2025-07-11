3db2ab4240071da4c9e05c5ec46822534ea241cc
Bashrixx <kaleliryan10081@gmail.com>
2025-07-11 12:18:46 +0300

    Js Changes

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
         slideIndex++;
         if (slideIndex > slides.length) {slideIndex = 1}
         slides[slideIndex - 1].style.display = 'block';
-        dots[slideIndex - 1].className += ' active';
+        dots[slideIndex - 1].className.add('active');
     }
 
     window.moveSlide = function(n) {
